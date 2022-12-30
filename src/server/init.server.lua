a3128ba3d8fa740b28113d38f354ad502ebb785c
chavez <chavezworkmail@gmail.com>
2022-12-29 20:08:59 -0600

    add lockers to data

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,7 +1,21 @@
+--[[
+
+    lockers are inside of properties (check to see if players have locker access, if not they have to buy)
+    -- group based
+    -- players can pick up guns when they have access to lockers
+    -- anyone in the group can buy the property with another group memebers approval
+
+]]
+
 local Sdk = require(script.Sdk)
 
 local options = {
-    defaultSchema = {},
+    defaultSchema = {
+        money = 0,
+        crystals = 0,
+        rank = 0,
+        lockers = {}, 
+    },
 }
 
 Sdk.init(options)
