01de0912ed6cb3fcfe625599017c22614e34c634
chavez <chavezworkmail@gmail.com>
2022-12-29 20:04:17 -0600

    add sdk player data

---
+++ src/server/Sdk.lua
@@ -0,0 +1,62 @@
+local Players = game:GetService("Players")
+local DataStoreService = game:GetService("DataStoreService")
+
+local Sdk = {
+    _playerData = {},
+    _playerDataStore = DataStoreService:GetDataStore("PlayerData"),
+}
+
+local function characterAdded(character)
+    
+end
+
+local function playerAdded(player)
+    local DEFAULT_SCHEMA = Sdk._defaultSchema
+
+    local playerData
+    local success, data = pcall(function()
+        return Sdk._playerDataStore:GetAsync(player.UserId)
+    end)
+
+    if success then
+        if data ~= nil then
+                playerData = data
+        else
+            playerData = DEFAULT_SCHEMA
+        end
+    else
+	playerData = DEFAULT_SCHEMA
+end
+
+    Sdk._playerData[player] = playerData
+end
+
+local function playerRemoving()
+    local playerData = Sdk._playerData[player]
+
+	local success, err = pcall(function()
+        -- save player data
+        self._playerDataStore:SetAsync(string.format("Player_%d", player.UserId), playerData)
+    end)
+
+    if (err) then
+        warn(err)
+    end
+
+    repeat
+        task.wait()
+    until success
+
+    Sdk._playerData[player] = nil
+end
+
+function Sdk.init(options)
+
+    sdk._defaultSchema = options.defaultSchema
+
+    Players.PlayerAdded:Connect(playerAdded)
+    Players.PlayerRemoving:Connect(playerRemoving)
+
+end
+
+return Sdk
