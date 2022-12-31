4a8021d930779bb47fb0bade90299dc14b035e1c
chavez <chavezworkmail@gmail.com>
2022-12-31 16:19:00 -0600

    add billboard button handler

---
+++ src/client/BillboardButtonHandler.client.lua
@@ -0,0 +1,40 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Common = ReplicatedStorage:WaitForChild("Common")
+
+local Comm = require(Common.Comm)
+
+local clientComm = Comm.ClientComm.new(ReplicatedStorage, false, "MainComm")
+local playerPressedBillboardButton = clientComm:GetSignal("PlayerPressedBillboardButton")
+
+local player = Players.LocalPlayer
+local playerGui = player.PlayerGui
+
+local function handleBillboardButtons(billboard)
+    for _, button in pairs(billboard:GetDescendants()) do
+        if not button:IsA("TextButton") then
+            continue
+        end
+        
+        button.MouseButton1Up:Connect(function()
+            local objectValue = billboard.Adornee
+            playerPressedBillboardButton:Fire(button.Name, objectValue)
+        end)
+    end
+end
+
+for _, billboard in pairs(playerGui:GetChildren()) do
+    if billboard:IsA("BillboardGui") then
+        handleBillboardButtons(billboard)
+    end
+end
+
+local function onBillboardAdded(billboard)
+    if billboard:IsA("BillboardGui") then
+        handleBillboardButtons(billboard)
+    end
+end
+
+playerGui.ChildAdded:Connect(onBillboardAdded)
+
